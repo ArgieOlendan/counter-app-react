1f07cc1e3789ab762a034100e459cb85da270e95
argie <argieolndn@gmail.com>
2020-12-13 14:17:51 +0800

    raising and handling event

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -12,13 +12,20 @@ class Counters extends Component {
     ],
   };
 
+  handleDelete = () => {
+    console.log("Event Handler Called");
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} selected={true}>
-            <h4>Counter #{counter.id}</h4>
-          </Counter>
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            value={counter.value}
+            selected={true}
+          />
         ))}
       </div>
     );
